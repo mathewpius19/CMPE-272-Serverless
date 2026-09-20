ed90e5eb90798c944d72be9aee2a04adeeb1e3dd
Sonit Kumar Pradhan <snkprdhn@gmail.com>
2026-09-20 16:49:01 -0700

    Create `ddb.tf`

---
+++ users/ddb.tf
@@ -0,0 +1,14 @@
+resource "aws_dynamodb_table" "users_table" {
+  name         = "${var.workshop_stack_base_name}_Users"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "userid"
+
+  attribute {
+    name = "userid"
+    type = "S"
+  }
+}
+
+output "UsersTable" {
+  value = aws_dynamodb_table.users_table.id
+}
