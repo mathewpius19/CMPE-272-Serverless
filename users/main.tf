56c852474aaa53c47a6c629a139a1c8a5ac5f5c5
Sagar Parker <sagar8parker@gmail.com>
2026-09-20 17:08:57 -0700

    Create `main.tf`

---
+++ users/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.65"
+    }
+    archive = {
+      source  = "hashicorp/archive"
+      version = "~> 2.8"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.3"
+    }
+  }
+}
+
+data "aws_region" "current" {}
+data "aws_caller_identity" "current" {}
